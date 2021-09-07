1042de98498bee40da37d8add560185058d96e35
robertvil <64606445+robertvil@users.noreply.github.com>
2021-09-07 22:26:48 +0300

    evening update
    evening update

---
+++ Udemy course/complete-javascript-course-master/01-Fundamentals-Part-1/starter/bmi_challenge.js
@@ -0,0 +1,20 @@
+
+
+const markMass = 78;
+const markHeight = 1.69;
+
+const johnMass = 120;
+const johnHeight = 1.95;
+
+const markBMI = markMass / (markHeight * markHeight);
+const johnBMI = johnMass / johnHeight ** 2;
+
+// const markHigherBMI = markBMI > johnBMI;
+
+// console.log(`Marks BMI is ${Math.round(markBMI)} and John's BMI is ${Math.round(johnBMI)}. Is Mark's BMI higher than Johns: ${markHigherBMI}`)
+
+// if (markBMI > johnBMI) {
+//     console.log(`Marks BMI is ${Math.round(markBMI)} and John's BMI is ${Math.round(johnBMI)}. Mark's BMI higher than John's!`)
+// } else {
+//     console.log(`Marks BMI is ${Math.round(markBMI)} and John's BMI is ${Math.round(johnBMI)}. John's BMI higher than Mark's`)
+// }
--- Udemy course/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ Udemy course/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -45,33 +45,135 @@
 // birthYear = 3434;
 
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
 
-console.log(ageJonas, ageSarah);
+// console.log(ageJonas, ageSarah);
 
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 
 
-const firstName = 'Jonas';
-const lastName = 'Schmedtmann'
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann'
 
-console.log(firstName + ' ' + lastName)
+// console.log(firstName + ' ' + lastName)
 
 
 // Assignment operators
-let x = 10;
-x += 5;
+// let x = 10;
+// x += 5;
 
-x++ // this will add  one to x so x = x + 1
+// x++; // this will add  one to x so x = x + 1
 
-x-- // this will subtract one from x so x = x - 1
-x--
+// x--; // this will subtract one from x so x = x - 1
+// x--;
 
-console.log(x)
+// console.log(x);
 
 // Comparison operators
 
-console.log(ageJonas < ageSarah);
-console.log(ageSarah >= 18);
+// console.log(ageJonas < ageSarah);
+// console.log(ageSarah >= 18);
+
+// console.log(now - 1991 < now - 2018);
+
+// let x, y;
+
+// x = y = 20 - 10 - 5;
+
+// console.log(x, y);
+
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
+// const firstName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const currentYear = 2037;
+
+// const jonas = "I'm " + firstName + ' a ' + (currentYear - birthYear) + ' years old ' + job + '.';
+
+// console.log(jonas);
+
+// // template literals
+// const jonasNew = `I'm ${firstName} a 
+// ${currentYear - birthYear} years old ${job}.`
+
+// console.log(jonasNew);
+
+// const age = 12;
+// const isOldEnough = age >= 18;
+
+// if (isOldEnough) {
+//     console.log('You are old enough');
+// } else {
+//     console.log('You are not old enough');
+// }
+
+// const age = 10;
+
+// if (age >= 18) {
+//     console.log('You are old enough to drive a car');
+// } else if (age >= 16) {
+//     console.log('You are old enough to drive a moped');
+// } else {
+//     const yearLeft = 18 - age;
+//     console.log(`You are not old enough to drive, you gave ${yearLeft} years/year before you can get driving license`);
+// }   
+
+
+// const demo = 110;
+
+// switch (demo) {
+//     case 112:
+//         console.log(`This is a large number ${demo}`)
+//         break;
+//     case 111:
+//         console.log(`This is a smaller number ${demo}`)
+//         break;
+//     default:
+//         console.log(`This is not a number ${demo}`)
+// }
+
+// // type conversion 
+// const inputYear = '1992';
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number('Hohohoh'));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// // type coercion
+
+// console.log('i am ' + 23 + ' years old');
+
+// console.log('23' +  '10' - 3);
+
+// console.log('23' * 20);
+// console.log('23' > '12');
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('johanas'));
+console.log(Boolean({})); // empty object 
+console.log(Boolean(''));
+
+const money = 10;
+if (money){
+    console.log("Don't spend it all ;)");
+} else{
+    console.log("You should get a job!");
+}
+
+let height;
+
+if (height){
+    console.log('Hight is defined');
+} else{
+    console.log('Hight id UNDEFINED!');
+}
