a9aa88a241c7d7fd6153e9c39821fa21d5a5ec59
robertvil <64606445+robertvil@users.noreply.github.com>
2021-09-07 10:48:14 +0300

    new sub rep

---
+++ Udemy course/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assigment.js
@@ -0,0 +1,12 @@
+
+// const country = "Finland";
+// const continent = "Europe";
+// let population = 5.5;
+
+// console.log(`I live in ${country}, it is located in northern part of ${continent}. Uor country population is ${population} million people!`);
+
+
+// const isIsland = false;
+// let language;
+
+// console.log(typeof isIsland, typeof language, typeof population, typeof country);
---
+++ Udemy course/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,77 @@
+  
+
+// let sj = 'amazing';
+// console.log(12 + 23423 + 3434 / 110);
+
+// console.log('Jonas');
+
+// let firstName = 'Jonas'
+
+// console.log(firstName + ' ' + 'Jonaitis');
+
+// console.log(true);
+
+// let javascriptIsFun = true;
+
+// console.log(javascriptIsFun);
+
+// console.log(typeof javascriptIsFun); // typeof shows the type of data 
+
+// let number = 12.090909;
+
+// console.log(typeof number); // typeof shows the type of data 
+
+// let empty;
+
+// console.log(typeof empty);
+
+// empty = 2323;
+
+// console.log(typeof empty);
+
+// console.log(typeof null);
+
+// const immutable = 34;
+
+// // immutable = 'do not change'
+
+// console.log(immutable);
+
+// let age = 20;
+// age = 30;
+
+// const birthYear = 1333; // when 'const' is used to declare a variable it can't be change latter, it is now a immutable variable
+
+// birthYear = 3434;
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann'
+
+console.log(firstName + ' ' + lastName)
+
+
+// Assignment operators
+let x = 10;
+x += 5;
+
+x++ // this will add  one to x so x = x + 1
+
+x-- // this will subtract one from x so x = x - 1
+x--
+
+console.log(x)
+
+// Comparison operators
+
+console.log(ageJonas < ageSarah);
+console.log(ageSarah >= 18);
