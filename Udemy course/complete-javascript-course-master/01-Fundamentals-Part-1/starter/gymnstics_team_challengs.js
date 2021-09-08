ce80584c7f85b3bb806ed8bdca3fbe6cc11e53cf
robertvil <64606445+robertvil@users.noreply.github.com>
2021-09-08 23:11:52 +0300

    update my work
    update my work

---
+++ Udemy course/complete-javascript-course-master/01-Fundamentals-Part-1/starter/gymnstics_team_challengs.js
@@ -0,0 +1,27 @@
+// Gymnastics teams average score challenge
+
+
+// const scoreDolphinsAverage = (96 + 108 + 89) / 3;
+// const scoreKoalasAverage = (88 + 91 + 110) / 3;
+
+// // Bonus 1
+// const scoreDolphinsAverage = Number((97 + 112 + 101) / 3);
+// const scoreKoalasAverage = Number((109 + 95 + 123) / 3);
+
+// // Bonus 2
+// const scoreDolphinsAverage = Number((97 + 112 + 101) / 3);
+// const scoreKoalasAverage = Number((109 + 95 + 106) / 3);
+
+
+// // check who won the competition with if statement
+// if (scoreDolphinsAverage === scoreKoalasAverage && scoreKoalasAverage >= 100 && scoreDolphinsAverage >= 100){
+//     console.log(`We have a draw between teams, no one wins, the score is ${scoreKoalasAverage}`);
+// } else if (scoreDolphinsAverage > scoreKoalasAverage && scoreDolphinsAverage >= 100){
+//     console.log(`Dolphins won the competition, wining score is ${scoreDolphinsAverage}!`);
+// } else if(scoreDolphinsAverage < scoreKoalasAverage && scoreKoalasAverage >= 100){
+//     console.log(`Koalas won the competition, wining score is ${scoreKoalasAverage}!`);
+// } else {
+//     console.log(`No one wins, the average score needs to bee at least 100. 
+//     Dolphins score: ${scoreDolphinsAverage}
+//     Koalas score: ${scoreKoalasAverage}`)
+// }
--- Udemy course/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ Udemy course/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -157,23 +157,3 @@
 
 // 5 falsy values: 0, '', undefined, null, NaN
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean('johanas'));
-console.log(Boolean({})); // empty object 
-console.log(Boolean(''));
-
-const money = 10;
-if (money){
-    console.log("Don't spend it all ;)");
-} else{
-    console.log("You should get a job!");
-}
-
-let height;
-
-if (height){
-    console.log('Hight is defined');
-} else{
-    console.log('Hight id UNDEFINED!');
-}
