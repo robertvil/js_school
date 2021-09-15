83ccc248256c45ea8294f00f6b530fbea5594fb5
robertvil <64606445+robertvil@users.noreply.github.com>
2021-09-15 23:50:34 +0300

    update

--- Udemy course/complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge_4.js
+++ Udemy course/complete-javascript-course-master/02-Fundamentals-Part-2/starter/challenge_4.js
@@ -1,18 +1,30 @@
-'use strick'
+// 'use strick'
 
-const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
-const tip = [];
-const totals = [];
+// const tip = [];
+// const totals = [];
 
-const calcTips = bill => bill >= 100 && bill <= 300 ? Math.round(bill * 0.15) : Math.round(bill * 0.20);
+// const calcTips = bill => bill >= 50 && bill <= 300 ? Math.round(bill * 0.15) : Math.round(bill * 0.20);
 
 
-for(let i = 0; i <= bill.length - 1; i++) {
-    let tipTemp = tip.push(calcTips(bill[i]));
-    totals.push(bill[i] + tipTemp);
-}
+// for(let i = 0; i < bill.length; i++) {
+//     let tipTemp = tip.push(calcTips(bill[i]));
+//     totals.push(bill[i] + tipTemp);
+// }
 
-console.log(calcTips(333));
-console.log(tip);
-console.log(totals);
+
+// console.log(bill);
+// console.log(tip);
+// console.log(totals);
+
+// function calcAverage(arr) {
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//         // console.log(sum);
+//     }
+//     return sum / arr.length
+// }
+
+// console.log(calcAverage(tip));
--- Udemy course/complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ Udemy course/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+
+console.log(222 - 222222);
+
+console.log(2323 - 434343);
